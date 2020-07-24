9da16929bfc67e27e6efffadc619c8e5c265bc56
Jeongmin <wjd326als@naver.com>
2020-07-25 01:16:43 +0900

    200724

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,19 +1,20 @@
 import React from "react";
 import "./Home.css";
 import { Link } from "react-router-dom";
+import background from "./img/background.jpg";
 
 class Home extends React.Component {
   render() {
     return (
       <main className="home_content">
         <div className="intro">
-          <div className="intro_info intro_item">
-            AIEZ is a perfect website for deep learning! <br></br>
-            Awesome! Beautiful! Amazing! Try this right now!
+          <div className="intro_info">
+            <div className="main_info">NET 챌린지 화이팅!!</div>
+            <div className="sub_info">
+              여러분 모두 힘내세요~ 코로나 싫어~! KOREN과 함께하는 ~ 집에 갈래!
+            </div>
           </div>
-          <div className="intro_image intro_item">
-            This will be filled with an awesome image!
-          </div>
+          <img className="intro_background" src={background} alt="img"></img>
         </div>
         <Link className="goToProject" to="/project/new_project">
           NOW GET STARTED!
