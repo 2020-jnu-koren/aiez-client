ef2d0d87e9da5d4c6f1ea8a9a7d50e9fa1bb4b83
Jeongmin <wjd326als@naver.com>
2020-11-12 01:11:34 +0900

    Updates

---
+++ precache-manifest.6d65cac964f69383b5e51776fbb06740.js
@@ -0,0 +1,118 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8bb2dec267c28933023fa5bfad0feb73",
+    "url": "/aiez-client/index.html"
+  },
+  {
+    "revision": "22030596b4f0ce799055",
+    "url": "/aiez-client/static/css/2.8718dd3e.chunk.css"
+  },
+  {
+    "revision": "7d78c5688d7d5736aba7",
+    "url": "/aiez-client/static/css/main.f52487be.chunk.css"
+  },
+  {
+    "revision": "22030596b4f0ce799055",
+    "url": "/aiez-client/static/js/2.67e7308b.chunk.js"
+  },
+  {
+    "revision": "d50512915a9d93353b354eb67f2df637",
+    "url": "/aiez-client/static/js/2.67e7308b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7d78c5688d7d5736aba7",
+    "url": "/aiez-client/static/js/main.37663a7c.chunk.js"
+  },
+  {
+    "revision": "a89d54c0e5441ce8056d",
+    "url": "/aiez-client/static/js/runtime-main.5fa4fdcd.js"
+  },
+  {
+    "revision": "b4892af1c587d30997a80e25b640a2c1",
+    "url": "/aiez-client/static/media/a1.b4892af1.PNG"
+  },
+  {
+    "revision": "0758d7c284424477795d3d6401b3c65d",
+    "url": "/aiez-client/static/media/a10.0758d7c2.PNG"
+  },
+  {
+    "revision": "f252d7aec2ca8583efd45086691d68c6",
+    "url": "/aiez-client/static/media/a11.f252d7ae.PNG"
+  },
+  {
+    "revision": "03e6e633b2b7bb1823dc7c09884a06ba",
+    "url": "/aiez-client/static/media/a12.03e6e633.PNG"
+  },
+  {
+    "revision": "63a7b54d6cb3cd3a819b0a9d364edac0",
+    "url": "/aiez-client/static/media/a13.63a7b54d.PNG"
+  },
+  {
+    "revision": "442847c69f6264e15ba9a82640f78351",
+    "url": "/aiez-client/static/media/a2.442847c6.PNG"
+  },
+  {
+    "revision": "5d9f87ac4cfdfe4dc2e9854a9477dbe9",
+    "url": "/aiez-client/static/media/a3.5d9f87ac.PNG"
+  },
+  {
+    "revision": "26524ee1ccea4aac7012721c2f64d2e9",
+    "url": "/aiez-client/static/media/a4.26524ee1.PNG"
+  },
+  {
+    "revision": "7cfb7b0891ccec9261a9c5f258fca82e",
+    "url": "/aiez-client/static/media/a5.7cfb7b08.PNG"
+  },
+  {
+    "revision": "dc001fd1fac3b563fdfe49571cb437a1",
+    "url": "/aiez-client/static/media/a6.dc001fd1.PNG"
+  },
+  {
+    "revision": "60c280b95818571febe5b0330f94e89f",
+    "url": "/aiez-client/static/media/a7.60c280b9.PNG"
+  },
+  {
+    "revision": "c2e4234b8aa309118b89759e8dcdca72",
+    "url": "/aiez-client/static/media/a8.c2e4234b.PNG"
+  },
+  {
+    "revision": "2d589d85a4bbbee74a51e2634a3a5dbc",
+    "url": "/aiez-client/static/media/a9.2d589d85.PNG"
+  },
+  {
+    "revision": "b9284b16e3cff92e0cc26083478430d1",
+    "url": "/aiez-client/static/media/area_selection.b9284b16.svg"
+  },
+  {
+    "revision": "68a7d581b8528fdedbb9489919dc2779",
+    "url": "/aiez-client/static/media/background.68a7d581.jpg"
+  },
+  {
+    "revision": "c7b1c1c244a468da3cb87dedd9d82839",
+    "url": "/aiez-client/static/media/bg.c7b1c1c2.png"
+  },
+  {
+    "revision": "17bff56d3aa820d53b7a65fc943b5f85",
+    "url": "/aiez-client/static/media/cursor.17bff56d.svg"
+  },
+  {
+    "revision": "8b44c690cd6a0d997e5dfdf9a99ebc8c",
+    "url": "/aiez-client/static/media/downArrow.8b44c690.svg"
+  },
+  {
+    "revision": "e49f23b67a53743f28eecdf9c6a21550",
+    "url": "/aiez-client/static/media/drag.e49f23b6.svg"
+  },
+  {
+    "revision": "88e4b7398f007f81b1f9d2394e74ef13",
+    "url": "/aiez-client/static/media/labeling.88e4b739.jpg"
+  },
+  {
+    "revision": "f329c3dbe3d52cc3310cfdb3ada0bb61",
+    "url": "/aiez-client/static/media/lastone.f329c3db.jpg"
+  },
+  {
+    "revision": "e26d72940bc883e2ec0ccddef55c9b84",
+    "url": "/aiez-client/static/media/upload.e26d7294.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/aiez-client/precache-manifest.ae49de162bec44d122bf624d61214499.js"
+  "/aiez-client/precache-manifest.6d65cac964f69383b5e51776fbb06740.js"
 );
 
 self.addEventListener('message', (event) => {
