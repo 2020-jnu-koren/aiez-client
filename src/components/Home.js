36b55f974dff2d99fc905739a2b589050b8588d0
Jeongmin <wjd326als@naver.com>
2020-11-13 01:53:03 +0900

    201113

--- src/components/Home.js
+++ src/components/Home.js
@@ -10,7 +10,7 @@ class Home extends React.Component {
         <div className="intro">
           <div className="intro_info">
             <div className="main_info">KOREN과 함께하면 어렵지 않아요!</div>
-            <div className="sub_info">초•중학생 대상 컨테이너 기반 클라우드 AI플랫폼</div>
+            <div className="sub_info">학생 대상 컨테이너 기반 클라우드 AI플랫폼</div>
           </div>
           <img className="intro_background" src={background} alt="img"></img>
         </div>
