fdab477cb34479db66ec4e371688096822b4c2ef
Jeongmin <wjd326als@naver.com>
2020-11-12 23:55:07 +0900

    201112

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,9 +1,70 @@
 import React from "react";
 import "./Project.css";
 import { Link } from "react-router-dom";
+import Dropzone from "react-dropzone";
+import classNames from "classnames";
+
+const imageMaxSize = 10000000; //bytes
+const acceptedFileTypes =
+  "image/x-png, image/png, image/jpg, image/jpeg, image/gif";
+const acceptedFileTypesArray = acceptedFileTypes.split(",").map((item) => {
+  return item.trim();
+});
 
 class Project extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      imgSrc: null,
+    };
+  }
+  verifyFile = (files) => {
+    if (files && files.length > 0) {
+      const currentFile = files[0];
+      const currentFileType = currentFile.type;
+      const currentFileSize = currentFile.size;
+      if (currentFileSize > imageMaxSize) {
+        alert(
+          "이 파일은 허용되지 않습니다." +
+          imageMaxSize +
+          "bytes이하의 파일을 업로드해주세요."
+        );
+        return false;
+      }
+      if (!acceptedFileTypesArray.includes(currentFileType)) {
+        alert("이미지 파일만 업로드 가능합니다.");
+        return false;
+      }
+      return true;
+    }
+  };
+  handleOnDrop = (files, rejectedFiles) => {
+    if (rejectedFiles && rejectedFiles.length > 0) {
+      this.verifyFile(rejectedFiles);
+    }
+    if (files && files.length > 0) {
+      const isVerified = this.verifyFile(files);
+      if (isVerified) {
+        //imageBase64Data
+        const currentFile = files[0];
+        const MyFileItemReader = new FileReader();
+        MyFileItemReader.addEventListener(
+          "load",
+          () => {
+            console.log(MyFileItemReader.result);
+            this.setState({
+              imgSrc: MyFileItemReader.result,
+            });
+          },
+          false
+        );
+        MyFileItemReader.readAsDataURL(currentFile);
+      }
+    }
+  };
+
   render() {
+    const { imgSrc } = this.state;
     return (
       <body>
         <header className="header">
@@ -32,6 +93,65 @@ class Project extends React.Component {
               <div className="project-btns">
                 <div className="project-train-btn project-btn">학습시키기</div>
                 <div className="project-test-btn project-btn">테스트</div>
+                {imgSrc !== null ? (
+                  <div>
+                    <Dropzone
+                      onDrop={this.handleOnDrop}
+                      accept={acceptedFileTypes}
+                      className="drop_zone"
+                    >
+                      {({ getRootProps, getInputProps, isDragActive }) => {
+                        return (
+                          <div
+                            {...getRootProps()}
+                            className={classNames("project-download-btn", {
+                              "dropzone--isActive": isDragActive,
+                            })}
+                          >
+                            <input {...getInputProps()} />
+                            {isDragActive ? (
+                              <p>Drop files here...</p>
+                            ) : (
+                                <div>
+                                  다운로드
+                                </div>
+                              )
+                            }
+                          </div>
+                        );
+                      }}
+                    </Dropzone>
+                  </div>
+                ) : (
+                    <div>
+                      <Dropzone
+                        onDrop={this.handleOnDrop}
+                        accept={acceptedFileTypes}
+                        className="drop_zone"
+                      >
+                        {({ getRootProps, getInputProps, isDragActive }) => {
+                          return (
+                            <div
+                              {...getRootProps()}
+                              className={classNames("project-download-btn", {
+                                "dropzone--isActive": isDragActive,
+                              })}
+                            >
+                              <input {...getInputProps()} />
+                              {isDragActive ? (
+                                <p>Drop files here...</p>
+                              ) : (
+                                  <div>
+                                    다운로드
+                                  </div>
+                                )
+                              }
+                            </div>
+                          );
+                        }}
+                      </Dropzone>
+                    </div>
+                  )}
               </div>
             </div>
           </div>
@@ -39,7 +159,7 @@ class Project extends React.Component {
             <div className="no_project">프로젝트가 존재하지 않습니다.</div>
           </div> */}
         </main>
-      </body>
+      </body >
     );
   }
 }
